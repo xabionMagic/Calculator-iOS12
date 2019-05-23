7f125d4d5ec763acc13c92a56a64c00143253127
Saroj Kunwar <rawnuk01@gmail.com>
2019-05-23 10:54:34 -0400

    Adding More Functionality, dealing with decimal, refer computed property and apply MVC.

---
+++ Calculator/CalculatorLogic.swift
@@ -0,0 +1,28 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Saroj on 5/23/19.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    var number: Double
+    
+    init(number: Double) {
+        self.number = number
+    }
+    
+    func calculate(symbol: String) -> Double? {
+        if symbol == "+/-" {
+            return number * -1
+        } else if symbol == "AC" {
+            return 0
+        } else if symbol == "%" {
+            return number * 0.01
+        }
+        return nil
+    }
+}
--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,24 +8,66 @@
 
 import UIKit
 
+import UIKit
+
 class ViewController: UIViewController {
     
+    private var isFinishedTypingNumber = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display lebel text to a Double.")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
+    }
+    
+    
     @IBOutlet weak var displayLabel: UILabel!
-    
-    
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        isFinishedTypingNumber = true
+        
+        if let calcMethod = sender.currentTitle {
+            let calculator = CalculatorLogic(number: displayValue)
+            
+            guard let result = calculator.calculate(symbol: calcMethod) else {
+                fatalError("The result of the calculation is nil.")
+            }
+            displayValue = result
+        }
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
         //What should happen when a number is entered into the keypad
-    
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                    
+                    
+                }
+                displayLabel.text = displayLabel.text! + numValue
+                // print("\(displayLabel.text)")
+            }
+        }
+        
     }
-
+    
 }
-
